2953c79b87c10d3e6cce13001854c01a3a276e25
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2021-05-26 22:13:38 -0300

    Added notification button

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.button import MDFillRoundFlatButton
 from nav import *
 from list_items import *
+from plyer import notification
+
+
+class NotificationButton(MDFillRoundFlatButton):
+    def notif(self):
+        notification.notify(title='New Notification', message='You just got notified!')
+        print("You were notified!")
+
+    pass
+
 
 class App(MDApp):
     def build(self):
